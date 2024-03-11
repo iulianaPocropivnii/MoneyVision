2bd86736f40d966aed695c4a53c91e1129b73552
danie <danie@DESKTOP-PPFHVI4>
2024-03-11 12:24:08 +0200

    register bl

---
+++ MoneyVision.Domain/Entities/User/URegisterData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoneyVision.Domain.Entities.User
+{
+    public class URegisterData
+    {
+        public string Credential { get; set; }
+        public string Password { get; set; }
+        public string RegisterIp { get; set; }
+        public DateTime RegisterDateTime { get; set; }
+    }
+}
