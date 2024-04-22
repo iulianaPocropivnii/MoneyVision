2026adf955f09a078502f7ea7836437a25061b18
danie <danie@DESKTOP-PPFHVI4>
2024-04-22 12:08:22 +0300

    fixed cookies rebase

--- MoneyVision.Domain/Entities/User/URegisterData.cs
+++ MoneyVision.Domain/Entities/User/URegisterData.cs
@@ -8,6 +8,7 @@ namespace MoneyVision.Domain.Entities.User
         public string Email { get; set; }
         public string Password { get; set; }
         public string RegisterIp { get; set; }
+        public string Email { get; set; }
         public DateTime RegisterDateTime { get; set; }
     }
 }
