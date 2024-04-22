6735fcb3e3ef87393c3bb39f5e5c5508de10da82
danie <danie@DESKTOP-PPFHVI4>
2024-04-22 12:27:27 +0300

    test

--- MoneyVision.Domain/Entities/User/Session.cs
+++ MoneyVision.Domain/Entities/User/Session.cs
@@ -8,20 +8,20 @@ using System.Threading.Tasks;
 
 namespace MoneyVision.Domain.Entities.User
 {
-    public class Session
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int SessionId { get; set; }
+        public class Session
+        {
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int SessionId { get; set; }
 
-        [Required]
-        [StringLength(30)]
-        public string Username { get; set; }
+            [Required]
+            [StringLength(30)]
+            public string Username { get; set; }
 
-        [Required]
-        public string CookieString { get; set; }
+            [Required]
+            public string CookieString { get; set; }
 
-        [Required]
-        public DateTime ExpireTime { get; set; }
+            [Required]
+            public DateTime ExpireTime { get; set; }
+        }
     }
-}
