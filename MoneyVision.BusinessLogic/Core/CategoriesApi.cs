5b2ebc44b36fae77d45322f0af65b866563f5bf6
danielaromanciuc <danielaromanciuc@gmail.com>
2024-05-27 20:49:04 +0300

    categories

---
+++ MoneyVision.BusinessLogic/Core/CategoriesApi.cs
@@ -0,0 +1,25 @@
+﻿using MoneyVision.Domain.Entities.Category.Responses;
+using MoneyVision.Domain.Entities.Category.Requests;
+using System;
+using System.Linq;
+using MoneyVision.BusinessLogic.DBModel;
+using MoneyVision.Domain.Entities.Workspace;
+using System.Collections.Generic;
+
+namespace MoneyVision.BusinessLogic.Core
+{
+
+    public class CategoriesApi
+    {
+        internal CategoriesListResp CategoriesListAction(CategoriesListData data)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var categories = db.Categories.Where(c => c.WorkspaceId == data.WorkspaceId).ToList();
+
+                return new CategoriesListResp { Status = true, Categories = categories };
+            }
+
+        }
+    }
+}
---
+++ MoneyVision.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using MoneyVision.Domain.Entities.Category.Requests;
+using MoneyVision.Web.Extension;
+
+using System.Web.Mvc;
+
+namespace MoneyVision.Web.Controllers
+{
+    public class CategoriesController : BaseController
+    {
+        public ActionResult Index(int workspaceId)
+        {
+            SessionStatus(workspaceId);
+            if ((string)System.Web.HttpContext.Current.Session["LoginStatus"] != "login")
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+
+            var profile = System.Web.HttpContext.Current.GetMySessionObject();
+
+            CategoriesListData categoriesListData = new CategoriesListData { WorkspaceId = profile.WorkspaceId };
+
+            var responseData = _session.CategoriesListAction(categoriesListData);
+
+            return View(responseData);
+        }
+    }
+}
