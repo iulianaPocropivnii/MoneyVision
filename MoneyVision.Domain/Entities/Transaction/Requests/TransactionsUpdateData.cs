b5b8ffb4d66f8ac9caba9dac02fd84613a4a7499
vladPopovschii <vlad.popovschii@gmail.com>
2024-06-03 22:34:35 +0300

    Implemented edit transaction

---
+++ MoneyVision.Domain/Entities/Transaction/Requests/TransactionsUpdateData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyVision.Domain.Entities.Transaction.Requests
+{
+     public class TransactionsUpdateData : TransactionsCreateData
+     {
+          public int Id { get; set; }
+     }
+}
---
+++ MoneyVision.Domain/Entities/Transaction/Responses/TransactionItemResp.cs
@@ -0,0 +1,16 @@
+﻿using MoneyVision.Domain.Entities.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyVision.Domain.Entities.Transaction.Responses
+{
+     public class TransactionItemResp : GenericResp
+     {
+          public Transaction Transaction {  get; set; }
+
+          public List<Category.Category> Categories { get; set; }
+     }
+}
